0f0420b4999ebb9964ab47dc1663d643d2c9a1a7
biQte <patrykgonet@gmail.com>
2023-11-17 12:47:04 +0100

    Add project files.

---
+++ pole-kola/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace pole_kola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liczenie pola koła");
+            Console.WriteLine("Podaj promień koła: ");
+            double radius = double.Parse(Console.ReadLine()); 
+
+            double pi = Math.Round((Math.PI), 2);
+
+            Console.WriteLine("Pole koła wynosi: " + Math.Round((pi * Math.Pow(radius, 2)), 2) + " jednostek kwadratowych");
+        }
+    }
+}
